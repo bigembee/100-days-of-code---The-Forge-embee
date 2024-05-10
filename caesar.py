faf4028beb2336bb1049056cab3bbe030b4bb50d
bigembee <goldenmonday@yahoo.com>
2024-05-10 23:41:19 +0100

    finished code

--- caesar.py
+++ caesar.py
@@ -1,10 +1,22 @@
 #Caesar cipher encryption method
 
 text = input('Enter text: ')
-shift = input('Enter shift key: ')
+shift = int(input('Enter shift key: '))
 text = text.lower()
-code = ""
+result = ""
+
 for char in text:
-    code = ord(char) + shift
-    newcode = chr(code)
-print(newcode)
+    #check for non-alphabets.
+    if char.isalpha():
+        code = ord(char) 
+        char_code = code + shift
+
+        if char_code > 121:
+            char_code -= 26
+
+        newcode = chr(char_code)
+        result += newcode
+        #print (char, code, char_code, newcode)
+    else:
+        result +=char
+print(f'here is your code: {result}')
